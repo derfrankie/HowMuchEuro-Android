0f4d8a47cb92b1344e417de46a63792a764f13cf
Frank Neulichedl <fn@frankie.bz>
2012-02-12 22:26:08 -0800

    First template version

---
+++ assets/www/js/app.js
@@ -0,0 +1,35 @@
+function onLoad(){
+          document.addEventListener("deviceready", onDeviceReady, true);
+     }
+function onDeviceReady(){
+          navigator.notification.alert("PhoneGap is working!!");
+     }
+
+var theScroll;
+function scroll(){
+    theScroll = new iScroll('wrapper');
+}
+ 
+document.addEventListener('DOMContentLoaded', scroll, false);
+
+$('#tab-bar a').on('click', function(e){
+    e.preventDefault();
+    var nextPage = $(e.target.hash);
+	page(nextPage); //You need to add this for it to work
+    $("#pages .current").removeClass("current");
+    nextPage.addClass("current");
+});
+
+
+function page(toPage) {
+    var toPage = $(toPage),
+    fromPage = $("#pages .current");
+    if(toPage.hasClass("current") || toPage === fromPage) {
+        return;
+    };
+    toPage.addClass("current fade in").one("webkitAnimationEnd", function(){
+        fromPage.removeClass("current fade out");
+        toPage.removeClass("fade in")
+    });
+    fromPage.addClass("fade out");
+}
